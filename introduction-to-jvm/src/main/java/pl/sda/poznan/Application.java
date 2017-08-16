262c31fe10baf8b731637e653496174fcbe98a95
Piotr <siwinski.piotr@outlook.com>
2017-08-16 19:30:46 +0200

    Added logger

--- introduction-to-jvm/src/main/java/pl/sda/poznan/Application.java
+++ introduction-to-jvm/src/main/java/pl/sda/poznan/Application.java
@@ -1,7 +1,12 @@
 package pl.sda.poznan;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Application {
     public static void main(String[] args) {
-        System.out.println("INTRODUCTION TO JVM");
+
+        log.info("Introduction to jvm");
+        log.info("Application started");
     }
 }
