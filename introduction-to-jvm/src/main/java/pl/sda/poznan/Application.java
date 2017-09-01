019c577aba4733f9d50e83696a6df84199e71551
Piotr <siwinski.piotr@outlook.com>
2017-09-01 10:03:19 +0200

    Updated pom.xml

--- introduction-to-jvm/src/main/java/pl/sda/poznan/Application.java
+++ introduction-to-jvm/src/main/java/pl/sda/poznan/Application.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class Application {
     public static void main(String[] args) {
 
-        log.info("Introduction to jvm");
-        log.info("Application started");
+        log.info("Works");
     }
 }
